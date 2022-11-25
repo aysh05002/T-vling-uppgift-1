9ab0cf51e3bb3f367ad7a3babd5b235b82a223f0
william <wiab03002@utb.vaxjo.se>
2022-11-25 09:10:34 +0100

    Add project files.

---
+++ Tävling uppgift 1/Tävling_uppgift_1.py
@@ -0,0 +1,26 @@
+tor=int(input("Tors tid ? "))
+mor=int(input("Mors tid ? "))
+
+y=40
+
+if tor>mor:
+    x=tor/mor
+    t=0
+    m=0
+    while y>1:
+        y-=(1+x*1)
+        t+=1
+        m+=1*x
+    print("Svar: Tor ", t, "Mor ", m)
+elif mor>tor:
+    x=mor/tor
+    t=0
+    m=0
+    while y>1:
+        y-=(1+x*1)
+        t+=1*x
+        m+=1
+    print("Svar: Tor ", t, "Mor ", m)
+
+else:
+    print("Savr:Tor 20, Mor 20")
