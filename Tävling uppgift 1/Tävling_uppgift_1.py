092ee76f5cfd02ca0c28b6e521af31ac26d9d6b7
aysh05002 <113014238+aysh05002@users.noreply.github.com>
2022-11-30 09:56:46 +0100

    Update Tävling_uppgift_1.py

--- Tävling uppgift 1/Tävling_uppgift_1.py
+++ Tävling uppgift 1/Tävling_uppgift_1.py
@@ -11,7 +11,7 @@ if tor>mor:
         y-=(1+x*1)
         t+=1
         m+=1*x
-    print("Svar: Tor ", t, "Mor ", m)
+    print("Svar: Tor ", int(t), "Mor ", int(m))
 elif mor>tor:
     x=mor/tor
     t=0
@@ -20,7 +20,7 @@ elif mor>tor:
         y-=(1+x*1)
         t+=1*x
         m+=1
-    print("Svar: Tor ", t, "Mor ", m)
+    print("Svar: Tor ", int(t), "Mor ", int(m))
 
 else:
     print("Savr:Tor 20, Mor 20")
